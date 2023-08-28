a069dd77583837dce6f4173e11b25d6dd965e2c4
Holger Emil Hansen <111877258+HolgerEmilHansen@users.noreply.github.com>
2023-08-28 10:05:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Fuck dig og velkommen i vores bølle bank. Sig mig kammerat, har du en konto?('ja','nej')");
+    }
+}
